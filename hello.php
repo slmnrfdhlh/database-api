a8e4f24fb743370c510733e2775397d9f28a4102
slmnrfdhlh <salmaafaadhilah123@gmail.com>
2018-11-04 21:21:18 +0700

    new

---
+++ hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<img src="me.jpg" width="250" height="300"><br><br>
+<?php 
+echo "Hai<br><br>";
+echo "Nama     : Salmaa Nur Faadhilah<br>";
+echo "Kelas    : XI RPL 3<br>";
+echo "Absen    : 13<br>"; ?>
+</body>
+</html>
